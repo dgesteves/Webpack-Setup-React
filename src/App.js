84e499f5d1c56d7181ed9e6f8e8d653e01ddfa1a
Diogo Esteves <diogo.esteves@mindera.com>
2019-08-16 23:46:23 +0100

    feat(bare minimum):

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+  return (
+      <div className='app'>
+        <div className='card'>
+          <h1 className='title'>
+            React is Working! 😀
+          </h1>
+        </div>
+      </div>
+  );
+};
+
+export default App;
